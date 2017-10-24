ea3f8432a869ec5de14dbb151d182abea8433ba0
Skylar Kelty <s.kelty@kent.ac.uk>
2017-10-24 16:59:18 +0100

    Fix python importing strangeness

--- env/slurm/examples/tensorflow.sh
+++ env/slurm/examples/tensorflow.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-user=USERNAME@kent.ac.uk
 #SBATCH --output=/home/USERNAME/slurm/logs/%j.out
 
-python /home/USERNAME/slurm/examples/tensorflow.py
+python /home/USERNAME/slurm/examples/tensorflowexample.py
