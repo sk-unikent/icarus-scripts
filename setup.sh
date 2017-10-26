ad25b279962d918d24306bc4bebc1500e102c491
Skylar Kelty <s.kelty@kent.ac.uk>
2017-10-26 13:11:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 
 # Setup environment (overwrites .bash* files).
 USERNAME=`logname`
-cp -R /opt/icarus/env/* ~/
+cp -R /opt/icarus/env/slurm ~/slurm
 cp -R /opt/icarus/env/.* ~/
 sed -i "s/USERNAME/$USERNAME/g" ~/slurm/examples/cpu.sh
 sed -i "s/USERNAME/$USERNAME/g" ~/slurm/examples/gpu.sh
