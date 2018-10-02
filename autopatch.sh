95f06858489a6f6894bcf3eb9cef89722d813315
Skylar Kelty <S.Kelty@kent.ac.uk>
2018-10-02 07:47:03 +0100

    Update autopatch.sh

--- autopatch.sh
+++ autopatch.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Puppet managed script
 
 /usr/bin/yum -q -y update
-sleep 1
-/sbin/grubby --default-kernel >/var/spool/nagios/next_kernel.txt
-sleep 1
-/opt/nrpe/check_kernel.sh || /sbin/reboot
+/sbin/reboot
